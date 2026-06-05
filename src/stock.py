2ae2003c9307d0a1c92da40fa94542d52e67b040
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2026-06-05 16:25:35 +0100

    passing third test_case

--- src/stock.py
+++ src/stock.py
@@ -1,4 +1,13 @@
+
 def classify_tub(tub):
-  tub = {}
+  tub["status"] = ""
+  capacity= tub["capacity"]
+  exixting_litres= tub["litres"]
+  percentage = (exixting_litres / capacity) * 100
+  percentage_form = f'{percentage}%'
+
+  if percentage > 70 :
+   tub["status"] = "ok"
+  
   return tub
 
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -3,8 +3,51 @@ from src.stock import classify_tub
 
 def test_classify_tub():
   # ARRANGE
-    tub = {}
+    modified_tub = {"flavour": "Vanilla Bean",
+    "litres": 4.5,
+    "capacity": 5.0,
+    "price_per_litre": 3.2}
   #ACT
-    result = classify_tub(tub)
+    result = classify_tub(modified_tub)
     #ASSERT
     assert type(result) == dict
+########################################
+
+def test_classify_tub_has_a_status():
+  # ARRANGE
+    modified_tub = {"flavour": "Vanilla Bean",
+    "litres": 4.5,
+    "capacity": 5.0,
+    "price_per_litre": 3.2}
+
+    #ACT
+    classify_tub(modified_tub )
+    status_case = "status" in modified_tub
+    #ASSERT
+    assert   status_case   ==  True
+
+
+
+# #############################################
+
+def test_classify_tub_has_a_status_ok():
+  # ARRANGE
+    modified_tub = {"flavour": "Vanilla Bean",
+    "litres": 4.5,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    "status":""
+    }
+
+
+    expected_result  = {"flavour": "Vanilla Bean",
+    "litres": 4.5,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    "status":"ok"
+    }
+   #ACT
+    result = classify_tub(modified_tub )
+
+    #ASSERT
+    assert  expected_result   ==  result
