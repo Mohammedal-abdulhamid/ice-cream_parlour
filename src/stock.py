f61ee9169e4c201a72bdcc0985a917de1ce4fe2c
Mohammedal-abdulhamid <mohammedalabdulhamid@gmail.com>
2026-06-05 16:43:04 +0100

    slove task 1

--- src/stock.py
+++ src/stock.py
@@ -1,13 +1,18 @@
 
 def classify_tub(tub):
-  tub["status"] = ""
-  capacity= tub["capacity"]
-  exixting_litres= tub["litres"]
+  modified_tub = tub.copy()
+  modified_tub["status"] = ""
+  capacity= modified_tub["capacity"]
+  exixting_litres= modified_tub["litres"]
   percentage = (exixting_litres / capacity) * 100
   percentage_form = f'{percentage}%'
 
   if percentage > 70 :
-   tub["status"] = "ok"
+   modified_tub["status"] = "ok"
+  elif 70 >= percentage > 25:
+   modified_tub["status"] = "low"
+  else:
+    modified_tub["status"] = "reorder"
   
-  return tub
+  return modified_tub
 
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -15,14 +15,13 @@ def test_classify_tub():
 
 def test_classify_tub_has_a_status():
   # ARRANGE
-    modified_tub = {"flavour": "Vanilla Bean",
+    tub = {"flavour": "Vanilla Bean",
     "litres": 4.5,
     "capacity": 5.0,
     "price_per_litre": 3.2}
 
     #ACT
-    classify_tub(modified_tub )
-    status_case = "status" in modified_tub
+    status_case = "status" in  classify_tub(tub)
     #ASSERT
     assert   status_case   ==  True
 
@@ -36,7 +35,7 @@ def test_classify_tub_has_a_status_ok():
     "litres": 4.5,
     "capacity": 5.0,
     "price_per_litre": 3.2,
-    "status":""
+    
     }
 
 
@@ -45,6 +44,51 @@ def test_classify_tub_has_a_status_ok():
     "capacity": 5.0,
     "price_per_litre": 3.2,
     "status":"ok"
+    }
+   #ACT
+    result = classify_tub(modified_tub )
+
+    #ASSERT
+    assert  expected_result   ==  result
+
+def test_classify_tub_has_a_status_low():
+  # ARRANGE
+    modified_tub = {"flavour": "Vanilla Bean",
+    "litres": 3.0,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    
+    }
+
+
+    expected_result  = {"flavour": "Vanilla Bean",
+    "litres": 3.0,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    "status":"low"
+    }
+   #ACT
+    result = classify_tub(modified_tub )
+
+    #ASSERT
+    assert  expected_result   ==  result
+
+##############################################
+def test_classify_tub_has_a_status_reorder():
+  # ARRANGE
+    modified_tub = {"flavour": "Vanilla Bean",
+    "litres": 1.0,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    
+    }
+
+
+    expected_result  = {"flavour": "Vanilla Bean",
+    "litres": 1.0,
+    "capacity": 5.0,
+    "price_per_litre": 3.2,
+    "status":"reorder"
     }
    #ACT
     result = classify_tub(modified_tub )
